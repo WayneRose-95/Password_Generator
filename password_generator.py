c4bc6cf603df9daf66f881a3d064a9c728be6061
Wayne Rose <e.guishardrose@hotmail.co.uk>
2021-12-21 04:00:42 +0000

    latest version using OOP

--- password_generator.py
+++ password_generator.py
@@ -1,18 +1,46 @@
 import string 
 import random 
 
-#TODO: Can this be refactored into a class now? 
+class Random_Password:
 
-def random_password(lower, upper, num, symbols):
-    length = int(input('Enter the length of your password:   '))
+    def __init__(self):
 
-    all = lower + upper + num + symbols
-    temp = random.sample(all, length)
-    password = "".join(temp)
-    return password 
+        self.lower  = string.ascii_lowercase 
+        self.upper = string.ascii_uppercase
+        self.digits = string.digits
+        self.puncutation = string.punctuation
+        
+
+    def get_user_input(self):
+        # print("Welcome to Password Generator! ")
+        self.user_input = int(input("Please enter your password length :"))
+
+        return self.user_input
 
 
-random_password(string.ascii_lowercase, 
-string.ascii_uppercase,
-string.digits, 
-string.punctuation)
+    def create_the_password(self, password_length : int):
+        letters = ""
+        for i in range(0,password_length):
+            letters += random.choice(self.lower + self.upper + self.digits + self.puncutation)
+        print(f'Your new password: {letters}')
+        return letters  
+        
+        
+
+    def choose_again(self):
+        r = input("Would you like to restart this program? Yes or No?")
+        if r == "yes" or r == "y":
+            new_password.create_the_password(new_password.get_user_input()) 
+            return True 
+
+        if r == "n" or r == "no":
+            print ("Enjoy your new password :)" )
+            return False 
+        
+   
+
+new_password = Random_Password()
+new_password.create_the_password(new_password.get_user_input())
+
+while new_password.choose_again() is True:
+    print(f'................................................')
